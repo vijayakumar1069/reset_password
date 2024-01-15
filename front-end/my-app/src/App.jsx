268733ae0bfd6f2aebb85241597f21a44a6324e6
vijayakumar1069 <vijay.r20799@gmail.com>
2024-01-15 11:49:14 +0530

    react- basic layout completed

--- front-end/my-app/src/App.jsx
+++ front-end/my-app/src/App.jsx
@@ -1,16 +1,21 @@
-import { useState } from 'react'
-
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Resetpassword from "./pages/Resetpassword";
 
 function App() {
- 
-
   return (
-   <>
-
-   
-   </>
-  
-  )
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/rest-password" element={<Resetpassword />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
-export default App
+export default App;
